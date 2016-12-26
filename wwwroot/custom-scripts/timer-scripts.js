109efa8cb3e8f0093bc45039b109ff8b9da57f95
Sergey Shpak <serge1272@mail.ru>
2016-12-26 18:17:00 +0100

    Add simple timer (stop functionality)

--- wwwroot/custom-scripts/timer-scripts.js
+++ wwwroot/custom-scripts/timer-scripts.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
     var end_date;
+    var time_left;
+    var was_stopped;
+    var is_active;
+    var timer_loop_id;
 
     var changeTimerValue;
     var getEndTime;
     var updateClock;
     var getTimerObj;
     var getTimerString;
+    var addZeroIfNeeded;
+    var freezeClock;
+
+    addZeroIfNeeded = function(time_str) {
+        if (time_str.length < 2) {
+            return "0" + time_str;
+        }
+        return time_str;
+    }
 
     changeTimerValue = function(val, el) {
         $(el).text(val);
@@ -40,21 +53,36 @@ $(document).ready(function() {
 
     getTimerString = function(time) {
         var timer_obj = getTimerObj(time);
-        var h = timer_obj.h.toString();
-        var m = timer_obj.m.toString();
-        var s = timer_obj.s.toString();
+        var h = addZeroIfNeeded(timer_obj.h.toString());
+        var m = addZeroIfNeeded(timer_obj.m.toString());
+        var s = addZeroIfNeeded(timer_obj.s.toString());
         return h + ":" + m + ":" + s;
     }
 
+    freezeClock = function() {
+        time_left = end_date - new Date();
+        clearInterval(timer_loop_id);
+    }
+
     $("#timer-start-btn").click(function() {
-        var interval = new Date(0, 0, 0, 0, 25, 0, 0);
+        var interval;
+        if (is_active) {
+            return;
+        }
+        is_active = true;
+        interval = new Date(0, 0, 0, 0, 25, 0, 0);
         setEndTime(interval);
         updateClock();
-        setInterval(updateClock, 1000);
+        timer_loop_id = setInterval(updateClock, 1000);
     });
 
     $("#timer-stop-btn").click(function() {
+        if (!is_active) {
+            return;
+        }
+        is_active = false;
         freezeClock();
+        was_stopped = true;
     });
 
     $("#timer-reset-btn").click(function() {
