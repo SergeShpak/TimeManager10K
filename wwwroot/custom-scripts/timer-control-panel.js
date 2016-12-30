802714b97b52f450dd5d9eaf670ddedc7de2cd36
Sergey Shpak <serge1272@mail.ru>
2016-12-30 11:47:21 +0100

    Add validation to time input

--- wwwroot/custom-scripts/timer-control-panel.js
+++ wwwroot/custom-scripts/timer-control-panel.js
@@ -1,7 +1,24 @@
-$(document).ready(function() {
-    var time_input = document.querySelector("#task-time-input");
+var validateTimeInput;
+var isValidAgainstRegex;
 
-    time_input.addEventListener('input', function(e) {
-        console.log("New input! Coming from this element: ", e.target);
+validateTimeInput = function() {
+    var time_input_el = document.querySelector("#task-time-input");
+    var time_str = time_input_el.value;
+    var is_valid = isValidAgainstRegex(time_str);
+    if (is_valid) {
+        console.log(time_str, " is a valid time string");
+        return;
+    }
+    console.log(time_str, " is not a valid time string");
+}
+
+isValidAgainstRegex = function(time_str) {
+    var is_valid = /^([0-9]{1,2}:)?([0-5]?[0-9]:)?([0-5]?[0-9])$/.test(time_str);
+    return is_valid;
+}
+
+$(document).ready(function() {
+    $("#task-set-btn").click(function() {
+        validateTimeInput();
     });
 });
