b2742fd47c3a05ff6d2618fae39fde51885010b3
Sergey Shpak <serge1272@mail.ru>
2017-01-16 21:10:21 +0100

    Add functionality to Task class

--- wwwroot/custom-scripts/task.js
+++ wwwroot/custom-scripts/task.js
@@ -40,3 +40,59 @@ Task.compare = function(first, second) {
     return interval_comparison || first.name.localeCompare(second.name)
             || start_time_comparison || end_time_comparison;
 };
+
+Task.local_storage_key = "timer_tasks";
+
+Task.prototype.saveToLocalStorage = function() {
+    var stored_tasks_json;
+    var stored_tasks;
+    var index_for_task;
+    var saveTasksToLocalStorage = function(tasks) {
+        var tasks_json = JSON.stringify(tasks);
+        localStorage[Task.local_storage_key] = tasks_json;
+        return;
+    };
+    if (!localStorage[Task.local_storage_key]) {
+        localStorage[Task.local_storage_key] = JSON.stringify([]);
+    }
+    stored_tasks_json = localStorage[Task.local_storage_key];
+    stored_tasks = JSON.parse(stored_tasks_json);
+    if (0 == stored_tasks.length) {
+        stored_tasks = stored_tasks.push(this);
+        saveTasksToLocalStorage(stored_tasks);
+        return;
+    }
+    index_for_task = findIndexForTask(stored_tasks);
+    stored_tasks.splice(index_for_task, 0);
+    saveTasksToLocalStorage(stored_tasks); 
+};
+
+Task.prototype.findIndexForTask = function(tasks) {
+    if (0 == tasks.length) {
+        return 0;
+    }
+    var start_index = 0;
+    var end_index = tasks.length - 1;
+    var index_found = start_index;
+    var current_task;
+    var current_index;
+    var compare_result;
+    while (start_index < end_index) {
+        current_index = Math.floor((end_index - start_index) / 2);
+        index_found = current_index;
+        current_task = tasks[current_index];
+        compare_result = Task.compare(this, current_task);
+        if (0 == compare_result) {
+            break;
+        }
+        if (compare_result > 0) {
+            start_index = current_index + 1;
+            continue;
+        }
+        if (compare_result < 0) {
+            end_index = current_index - 1;
+            continue;
+        }
+    }
+    return index_found;
+};
